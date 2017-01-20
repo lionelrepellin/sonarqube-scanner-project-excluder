b0270daf0392a888620720981ce5b7188fc7c194
Lionel <lionel.repellin@gmail.com>
2017-01-20 07:46:21 +0100

    initial commit

---
+++ ProjectExcluder/ClassLibraryA/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryA
+{
+    public class Class1
+    {
+    }
+}
---
+++ ProjectExcluder/ProjectExcluder/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ProjectExcluder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if (args.Count() == 1)
+            //{
+
+            //var fullSourceFilePath = args.Single();
+            var fullSourceFilePath = Path.Combine(Environment.CurrentDirectory, "monprojet.csproj");
+
+            if (!File.Exists(fullSourceFilePath))
+                throw new FileNotFoundException("not found !");
+
+            var baseDirectory = Path.GetDirectoryName(fullSourceFilePath);
+
+            var doc = AddSonarQubeElement(fullSourceFilePath);
+
+            doc.Save(fullSourceFilePath);
+
+            Console.WriteLine("End");
+            Console.Read();
+            //}
+
+        }
+
+        private static void FindChildProjects(string filePath)
+        {
+            var document = XDocument.Load(filePath);
+
+            foreach (var element in document.Root.Elements())
+            {
+                if (element.Name.LocalName == "PropertyGroup" && !element.HasAttributes && element.HasElements) // && !firstPropertyGroupFound)
+                {
+                    element.Add(new XElement(parentNameSpace + "SonarQubeExclude", true));
+                    //firstPropertyGroupFound = true;
+                    break;
+                }
+            }
+        }
+
+        private static XDocument AddSonarQubeElement(string filePath)
+        {
+            var document = XDocument.Load(filePath);
+            var parentNameSpace = document.Root.Name.Namespace;
+            //var firstPropertyGroupFound = false;
+
+            foreach (var element in document.Root.Elements())
+            {
+                if (element.Name.LocalName == "PropertyGroup" && !element.HasAttributes && element.HasElements) // && !firstPropertyGroupFound)
+                {
+                    element.Add(new XElement(parentNameSpace + "SonarQubeExclude", true));
+                    //firstPropertyGroupFound = true;
+                    break;
+                }
+            }
+
+            return document;
+        }
+    }
+}
