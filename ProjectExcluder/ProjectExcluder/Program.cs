ab4e0b92c516c0abaad482c5a2d4d4e6ddaec1fd
git <lionel.repellin@gmail.com>
2017-01-20 15:12:03 +0100

    To be continued...

--- ProjectExcluder/ProjectExcluder/Program.cs
+++ ProjectExcluder/ProjectExcluder/Program.cs
@@ -9,63 +9,136 @@ using System.Xml.Linq;
 
 namespace ProjectExcluder
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //if (args.Count() == 1)
-            //{
+	class Program
+	{
+		static int counter = 0;
 
-            //var fullSourceFilePath = args.Single();
-            var fullSourceFilePath = Path.Combine(Environment.CurrentDirectory, "monprojet.csproj");
+		static void Main(string[] args)
+		{
+			var mainProjectFilePath = string.Empty;
+			
+#if DEBUG
+			mainProjectFilePath = @"C:\dvlp\Project\ASPNET4\Report2\Report2.Web\Report2.Web.csproj";
+#else
+			if (args.Count() != 1)
+				return;
 
-            if (!File.Exists(fullSourceFilePath))
-                throw new FileNotFoundException("not found !");
+			mainProjectFilePath = args[0];
+#endif
 
-            var baseDirectory = Path.GetDirectoryName(fullSourceFilePath);
+			if (!File.Exists(mainProjectFilePath))
+				throw new FileNotFoundException("not found !");
 
-            var doc = AddSonarQubeElement(fullSourceFilePath);
+			var document = XDocument.Load(mainProjectFilePath);
 
-            doc.Save(fullSourceFilePath);
+			FindChildProjects(mainProjectFilePath);
 
-            Console.WriteLine("End");
-            Console.Read();
-            //}
+#if DEBUG
+			Console.WriteLine($"Save main project file: {Path.GetFileName(mainProjectFilePath)}");
+#else
+			document.Save(mainProjectFilePath);
+#endif
+			Console.WriteLine("End");
+			Console.Read();
+			//}
 
-        }
+		}
 
-        private static void FindChildProjects(string filePath)
-        {
-            var document = XDocument.Load(filePath);
+		private static void FindChildProjects(string filePath)
+		{
+			//counter++;
 
-            foreach (var element in document.Root.Elements())
-            {
-                if (element.Name.LocalName == "PropertyGroup" && !element.HasAttributes && element.HasElements) // && !firstPropertyGroupFound)
-                {
-                    element.Add(new XElement(parentNameSpace + "SonarQubeExclude", true));
-                    //firstPropertyGroupFound = true;
-                    break;
-                }
-            }
-        }
+			var di = new DirectoryInfo(filePath);
+			filePath = di.FullName;
 
-        private static XDocument AddSonarQubeElement(string filePath)
-        {
-            var document = XDocument.Load(filePath);
-            var parentNameSpace = document.Root.Name.Namespace;
-            //var firstPropertyGroupFound = false;
+			var document = XDocument.Load(filePath);
+			var baseDirectory = Path.GetDirectoryName(filePath);
+			
+			Console.WriteLine(C($"FindChildProjects from: {Path.GetFileName(filePath)}"));
+			/*
+			foreach (var itemGroup in document.Root.Elements().Where(e => e.Name.LocalName == "ItemGroup" && !e.HasAttributes && e.HasElements))
+			{
+				var contentElements = ContainsContentElement(itemGroup);
+				if (contentElements.Any())
+				{
+					Console.WriteLine(C($"We have {referenceElements.Count()} Project reference element(s)"));
 
-            foreach (var element in document.Root.Elements())
-            {
-                if (element.Name.LocalName == "PropertyGroup" && !element.HasAttributes && element.HasElements) // && !firstPropertyGroupFound)
-                {
-                    element.Add(new XElement(parentNameSpace + "SonarQubeExclude", true));
-                    //firstPropertyGroupFound = true;
-                    break;
-                }
-            }
+					foreach(var contentElement in contentElements.Where(c => c.Attribute("Include").Value.Contains(".csproj")))
+					{
+						var childProjectFullPath = Path.Combine(baseDirectory, ExtractProjectFile(contentElement));
+						AddSonarQubeElement(childProjectFullPath);
+						FindChildProjects(childProjectFullPath);
+					}
+				}
+			}
+			*/
+			foreach (var itemGroup in document.Root.Elements().Where(e => e.Name.LocalName == "ItemGroup" && !e.HasAttributes && e.HasElements))
+			{
+				var referenceElements = ContainsReferenceElement(itemGroup);
+				if (referenceElements.Any())
+				{
+					Console.WriteLine(C($"We have {referenceElements.Count()} Project reference included in {Path.GetFileName(filePath)}"));
 
-            return document;
-        }
-    }
+					foreach (var contentElement in referenceElements.Where(c => c.Attribute("Include").Value.Contains(".csproj")))
+					{
+						var childProjectFullPath = Path.Combine(baseDirectory, ExtractProjectFile(contentElement));
+						AddSonarQubeElement(childProjectFullPath);
+						FindChildProjects(childProjectFullPath);
+					}
+				}
+			}
+		}
+
+		private static string ExtractProjectFile(XElement content)
+		{
+			return content.Attribute("Include").Value;
+		}
+
+		private static IEnumerable<XElement> ContainsContentElement(XElement itemGroup)
+		{
+			return itemGroup.Elements().Where(e => e.Name.LocalName == "Content" && e.HasAttributes).ToList();
+		}
+
+		private static IEnumerable<XElement> ContainsReferenceElement(XElement itemGroup)
+		{
+			return itemGroup.Elements().Where(e => e.Name.LocalName == "ProjectReference" && e.HasAttributes).ToList();
+		}
+
+		private static XDocument AddSonarQubeElement(string filePath)
+		{
+			var document = XDocument.Load(filePath);
+			var parentNameSpace = document.Root.Name.Namespace;
+
+			var propertyGroup = document.Root.Elements().Where(e => e.Name.LocalName == "PropertyGroup" && !e.HasAttributes && e.HasElements).FirstOrDefault();
+
+			var sonarQubeAttribute = propertyGroup.Elements().SingleOrDefault(p => p.Name.LocalName == "SonarQubeExclude");
+
+			if(sonarQubeAttribute == null)
+			{
+#if DEBUG
+				Console.WriteLine(C($"Add SonarQubeExclude attribute to: {Path.GetFileName(filePath)}"));
+#else
+				propertyGroup.Add(new XElement(parentNameSpace + "SonarQubeExclude", true));
+#endif
+			}
+
+#if DEBUG
+			Console.WriteLine(C($"Save project file: {Path.GetFileName(filePath)}"));
+#else
+			document.Save(filePath);
+#endif
+
+			return document;
+		}
+
+		private static string C(string msg)
+		{
+			var s = string.Empty;
+			for (int x = 0; x < counter; x++)
+			{
+				s += '-';
+			}
+			return $"{s}> {msg}";
+		}
+	}
 }
