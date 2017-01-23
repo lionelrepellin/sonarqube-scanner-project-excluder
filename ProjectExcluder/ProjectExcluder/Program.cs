172a01ccb10a86711174748abc69445fa827749c
git <lionel.repellin@gmail.com>
2017-01-23 14:17:25 +0100

    Just some adjustments

--- ProjectExcluder/ProjectExcluder/Program.cs
+++ ProjectExcluder/ProjectExcluder/Program.cs
@@ -9,104 +9,103 @@ using System.Xml.Linq;
 
 namespace ProjectExcluder
 {
-    class Program
-    {
-        private static int _processedFilesCount = 0;
+	class Program
+	{
+		private static int _processedFilesCount = 0;
 
-        static void Main(string[] args)
-        {
-            if (args.Count() != 1)
-                return;
+		static void Main(string[] args)
+		{
+			if (args.Count() != 1)
+				return;
 
-            var mainProjectFilePath = args[0];
+			var mainProjectFilePath = args[0];
 
-            if (!File.Exists(mainProjectFilePath))
-                throw new FileNotFoundException($"Not found: {mainProjectFilePath}");
+			if (!File.Exists(mainProjectFilePath))
+				throw new FileNotFoundException($"Not found: {mainProjectFilePath}");
 
-            FindChildProjects(mainProjectFilePath);
+			FindChildProjects(mainProjectFilePath);
 
-            Console.WriteLine($"The End: {_processedFilesCount} files have been processed");
-            Console.Read();
-        }
+			Console.WriteLine($"The End: {_processedFilesCount} files have been processed");
+#if DEBUG
+			Console.Read();
+#endif
+		}
 
-        private static void FindChildProjects(string filePath)
-        {
-            filePath = new DirectoryInfo(filePath).FullName;
+		private static void FindChildProjects(string filePath)
+		{
+			filePath = new DirectoryInfo(filePath).FullName;
 
-            var baseDirectory = Path.GetDirectoryName(filePath);
-            var document = XDocument.Load(filePath);
-            var itemGroups = FindItemGroupElements(document);
+			var baseDirectory = Path.GetDirectoryName(filePath);
+			var document = XDocument.Load(filePath);
+			var itemGroups = FindItemGroupElements(document);
 
-            foreach (var itemGroup in itemGroups)
-            {
-                var projectReferences = FindProjectReferenceElements(itemGroup);
-                if (projectReferences.Any())
-                {
-                    foreach (var projectReference in projectReferences)
-                    {
-                        var childProjectPath = new DirectoryInfo(Path.Combine(baseDirectory, projectReference.Attribute("Include").Value)).FullName;
+			foreach (var itemGroup in itemGroups)
+			{
+				var projectReferences = FindProjectReferenceElements(itemGroup);
+				if (projectReferences.Any())
+				{
+					foreach (var projectReference in projectReferences)
+					{
+						var childProjectFilePath = new DirectoryInfo(Path.Combine(baseDirectory, projectReference.Attribute("Include").Value)).FullName;
 
-                        if (!File.Exists(childProjectPath))
-                            throw new FileNotFoundException($"Not found: {childProjectPath}");
+						if (!File.Exists(childProjectFilePath))
+							throw new FileNotFoundException($"Not found: {childProjectFilePath}");
 
-                        AddSonarQubeElement(childProjectPath);
-                        FindChildProjects(childProjectPath);
-                    }
-                }
-            }
-        }
+						AddSonarQubeElement(childProjectFilePath);
+						FindChildProjects(childProjectFilePath);
+					}
+				}
+			}
+		}
 
-        private static void AddSonarQubeElement(string filePath)
-        {
-            var document = XDocument.Load(filePath);
-            var parentNameSpace = document.Root.Name.Namespace;
-            var propertyGroup = FindPropertyGroup(document);
+		private static void AddSonarQubeElement(string filePath)
+		{
+			var document = XDocument.Load(filePath);
+			var propertyGroup = FindPropertyGroup(document);
 
-            if (!IsFileAlreadyUpdated(propertyGroup))
-            {
-                propertyGroup.Add(new XElement(parentNameSpace + "SonarQubeExclude", true));
-                document.Save(filePath);
+			if (!IsFileAlreadyUpdated(propertyGroup))
+			{
+				propertyGroup.Add(new XElement(document.Root.Name.Namespace + "SonarQubeExclude", true));
+				document.Save(filePath);
 
-                _processedFilesCount++;
-                Console.WriteLine($"Add SonarQubeExclude attribute to: {Path.GetFileName(filePath)}");
-            }
-        }
+				_processedFilesCount++;
+				Console.WriteLine($"Add SonarQubeExclude attribute to: {Path.GetFileName(filePath)}");
+			}
+		}
 
-        private static bool IsFileAlreadyUpdated(XElement propertyGroup)
-        {
-            return propertyGroup.Elements()
-                                .Any(p => p.Name.LocalName == "SonarQubeExclude");
-        }
+		private static bool IsFileAlreadyUpdated(XElement propertyGroup)
+		{
+			return propertyGroup.Elements()
+								.Any(p => p.Name.LocalName == "SonarQubeExclude");
+		}
 
-        private static IEnumerable<XElement> FindProjectReferenceElements(XElement itemGroup)
-        {
-            return itemGroup.Elements()
-                            .Where(e =>
-                                e.Name.LocalName == "ProjectReference" &&
-                                e.HasAttributes &&
-                                e.Attribute("Include") != null &&
-                                e.Attribute("Include").Value.Contains(".csproj")
-            ).ToList();
-        }
+		private static IEnumerable<XElement> FindProjectReferenceElements(XElement itemGroup)
+		{
+			return itemGroup.Elements()
+							.Where(e =>
+								e.Name.LocalName == "ProjectReference" &&
+								e.HasAttributes &&
+								e.Attribute("Include") != null &&
+								e.Attribute("Include").Value.Contains(".csproj"));
+		}
 
-        private static IEnumerable<XElement> FindItemGroupElements(XDocument document)
-        {
-            return document.Root.Elements()
-                                .Where(e =>
-                                    e.Name.LocalName == "ItemGroup" &&
-                                    !e.HasAttributes &&
-                                    e.HasElements
-            ).ToList();
-        }
+		private static IEnumerable<XElement> FindItemGroupElements(XDocument document)
+		{
+			return document.Root.Elements()
+								.Where(e =>
+									e.Name.LocalName == "ItemGroup" &&
+									!e.HasAttributes &&
+									e.HasElements);
+		}
 
-        private static XElement FindPropertyGroup(XDocument document)
-        {
-            return document.Root.Elements()
-                                .FirstOrDefault(e =>
-                                    e.Name.LocalName == "PropertyGroup" &&
-                                    !e.HasAttributes && 
-                                    e.HasElements
-            );
-        }
-    }
+		private static XElement FindPropertyGroup(XDocument document)
+		{
+			return document.Root.Elements()
+								.FirstOrDefault(e =>
+									e.Name.LocalName == "PropertyGroup" &&
+									!e.HasAttributes &&
+									e.HasElements
+			);
+		}
+	}
 }
